bd052eae3baf35384a7d355ae5f1a3d823ae30d8
Daniel Broughan <root@Daniels-MacBook-Pro.local>
2016-02-11 10:23:11 -0600

    Now not case sensitive

--- us-canada-zip-codes.js
+++ us-canada-zip-codes.js
@@ -2,7 +2,7 @@
 
 // The RegExp for US zip codes and Canadian Postal codes
 var usZip = new RegExp(/\b\d{4,5}(-\d{4,5})?\b/);
-var canZip = new RegExp(/\b[A-Z]{1}\d{1}[A-Z]{1}\s+\d{1}[A-Z]{1}\d{1}\b/);
+var canZip = new RegExp(/\b[A-Za-z]{1}\d{1}[A-Za-z]{1}\s+\d{1}[A-Za-z]{1}\d{1}\b/);
 
 // Once we've set the variables, we can access them from any string:
 var str = "123 Fake St. Chicago 60601";
